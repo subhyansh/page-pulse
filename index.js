14f9c839e2e762659c5ad0c1fb9d7436e332f7ef
subhyansh <official.singhsubhyansh@gmail.com>
2026-07-24 18:31:39 +0530

    Setup Express server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Welcome to Page Pulse");
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Page Pulse 🚀");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
